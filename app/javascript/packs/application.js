743f3df86e5eef34fecfa4f49836bfafe7b6b43c
Kei <kt10195665@yahoo.ne.jp>
2021-12-30 18:18:03 +0900

    Add: 音声機能のjsファイルを作成 #6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "./games/index.js"
+import "./users/index"
 
 Rails.start()
 Turbolinks.start()
