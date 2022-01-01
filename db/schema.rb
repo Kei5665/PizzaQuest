c0414a3ac09f9f562716ec6e05da92587d91150a
Kei <kt10195665@yahoo.ne.jp>
2022-01-01 09:14:34 +0900

    Add: monster_recipeモデルを追加 #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_062043) do
+ActiveRecord::Schema.define(version: 2022_01_01_001256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "monster_recipes", force: :cascade do |t|
+    t.bigint "monster_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["monster_id"], name: "index_monster_recipes_on_monster_id"
+    t.index ["recipe_id"], name: "index_monster_recipes_on_recipe_id"
+  end
 
   create_table "monsters", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_12_31_062043) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "monster_recipes", "monsters"
+  add_foreign_key "monster_recipes", "recipes"
 end
