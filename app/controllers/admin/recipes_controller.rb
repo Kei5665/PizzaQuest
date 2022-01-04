7f99435da57bbc59fef65e2e6b2887dd80040946
Kei <kt10195665@yahoo.ne.jp>
2022-01-04 15:26:10 +0900

    Add: レシピ作成画面、一覧画面を作成 #12

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,7 +1,24 @@
 class Admin::RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to admin_recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :gram)
   end
 end
