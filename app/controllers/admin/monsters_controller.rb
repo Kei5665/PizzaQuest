cd5f31f42ba6354c8cadbb6af1ec37ff93b00a41
Kei <kt10195665@yahoo.ne.jp>
2022-01-04 12:46:41 +0900

    Fix: 間違い部分を修正 #12

--- app/controllers/admin/monsters_controller.rb
+++ app/controllers/admin/monsters_controller.rb
@@ -5,4 +5,19 @@ class Admin::MonstersController < ApplicationController
   def new
     @monster = Monster.new
   end
+
+  def create
+    @monster = Monster.new(monster_params)
+    if @monster.save
+      redirect_to admin_monsters_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def monster_params
+    params.require(:monster).permit(:name, :level, :detail)
+  end
 end
