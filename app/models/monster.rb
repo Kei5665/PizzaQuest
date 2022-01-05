075cd58f45e60947b25588f4817125b0430ab2b2
Kei <kt10195665@yahoo.ne.jp>
2022-01-05 15:34:44 +0900

    Add: monsterモデル、recipeモデルにバリデーションを追加 #12

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,6 @@
 class Monster < ApplicationRecord
   has_many :monster_recipes, dependent: :destroy
   has_many :recipes, through: :monster_recipes, source: :recipe
+  validates :name, presence: true
+  validates :level, presence: true
 end
