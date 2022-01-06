11e58638097338e3ab62d96ddc8f42f95436e205
Kei <kt10195665@yahoo.ne.jp>
2022-01-06 13:57:37 +0900

    Add: モンスターに紐づいたレシピの削除 #14

--- app/controllers/admin/monster_recipes_controller.rb
+++ app/controllers/admin/monster_recipes_controller.rb
@@ -15,6 +15,12 @@ class Admin::MonsterRecipesController < ApplicationController
     redirect_to admin_monsters_path
   end
 
+  def destroy
+    monster_recipe = MonsterRecipe.find(params[:id])
+    monster_recipe.destroy!
+    redirect_to admin_monsters_path
+  end
+
   private
 
   def monster_recipe_params
