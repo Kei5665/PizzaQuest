0029e1511719b0f67194853579c09de186be6258
Kei <kt10195665@yahoo.ne.jp>
2022-01-11 19:57:01 +0900

    Add: 終了画面のざっくりとしたviewとロジックを作成 #20

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,5 +12,9 @@ class GamesController < ApplicationController
   def result
     @monster = Monster.find_by(id: params[:monster_id])
     @gold = @monster.level * 100
+
+    @level_current = Level.first
+    @level_next = Level.second
+    @require_point = @level_next.required_gold - @level_current.required_gold
   end
 end
