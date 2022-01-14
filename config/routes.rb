b2b4a92eb7ed5fbb1e472a7c299090e1f7f38d92
Kei <kt10195665@yahoo.ne.jp>
2022-01-14 13:02:56 +0900

    Add: game#indexのルーティングを作成 #22

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'games#index'
   get '/games/result', to: 'games#result'
+  get '/games', to: 'games#index'
 
   namespace :admin do 
     resources :monsters
