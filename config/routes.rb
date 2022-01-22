3fec7c27e5871b2c72cd9465b87cd53c87d82ace
Kei <kt10195665@yahoo.ne.jp>
2022-01-23 03:40:16 +0900

    Add: 初級の練習用のコントローラー、view、ルーティングを作成 #32

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'monsters#index'
   get '/games/result', to: 'games#result'
   get '/games', to: 'games#index'
+  get '/beginner/practice', to: 'beginner/practice#index'
 
   namespace :admin do 
     resources :monsters
