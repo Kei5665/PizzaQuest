30c3b4d43027ae26b4a2f5ecf64552d6b63dfcac
Kei <kt10195665@yahoo.ne.jp>
2022-01-23 16:26:57 +0900

    Add: モンスターの名前も表示するようにする #32

--- app/controllers/beginner/practice_controller.rb
+++ app/controllers/beginner/practice_controller.rb
@@ -2,20 +2,23 @@ class Beginner::PracticeController < Beginner::BaseController
   def index
     monsters = Monster.all.where(level: 1)
 
-    @monsterss = []
+    monster_recipes = []
+    monster_names = []
 
     monsters.each do |monster|
+            
       recipe_arr = []
-
       recipe_order = monster.recipes.order(:order_num)
       recipe_order.each do |recipe|
         recipe_arr.push(recipe.name)
       end
 
-      @monsterss.push(recipe_arr)
+      monster_recipes.push(recipe_arr)
+      monster_names.push(monster.name)
     end
 
-    gon.monsters = @monsterss
-
+    gon.monster_recipes = monster_recipes
+    gon.monster_names = monster_names
+    
   end
 end
