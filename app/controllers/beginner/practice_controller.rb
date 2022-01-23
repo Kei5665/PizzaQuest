ada79a6344882c1f6bb198e73b46aa095aa8c980
Kei <kt10195665@yahoo.ne.jp>
2022-01-23 15:29:09 +0900

    Add: 並べ替えクイズにbeginnerコントローラーの変数を入れることに成功 #32

--- app/controllers/beginner/practice_controller.rb
+++ app/controllers/beginner/practice_controller.rb
@@ -1,4 +1,21 @@
 class Beginner::PracticeController < Beginner::BaseController
   def index
+    monsters = Monster.all.where(level: 1)
+
+    @monsterss = []
+
+    monsters.each do |monster|
+      recipe_arr = []
+
+      recipe_order = monster.recipes.order(:order_num)
+      recipe_order.each do |recipe|
+        recipe_arr.push(recipe.name)
+      end
+
+      @monsterss.push(recipe_arr)
+    end
+
+    gon.monsters = @monsterss
+
   end
 end
