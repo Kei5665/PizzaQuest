61bc3ea4b201ad2fcf5078b2ca37c74ec15ea68d
Kei <kt10195665@yahoo.ne.jp>
2022-01-31 00:26:18 +0900

    Delete: スタートモーダルは削除 #33

--- app/javascript/packs/start_modal.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('turbolinks:load', () => {
-
-//   var myModal = document.getElementById('dialog-rounded');
-//   document.onreadystatechange = function () {
-//     myModal.showModal()
-//   };
-  
-//   })
