8ff4c50211c34919bc48c4a708ba2c910f528be6
Kei <kei@KeinoMacBook-Air.local>
2022-02-10 22:32:39 +0900

    Add: トライアルモードの結果画面を作成 #46

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -7,4 +7,13 @@ class TrialsController < ApplicationController
     gon.grams = @monster.gram_arr
     gon.answer_grams = @monster.answer_grams
   end
+
+  def result
+    @monster = Monster.find_by(id: params[:monster_id])
+    @gold = @monster.calculate_gold
+
+    @level_current = Level.first
+    @level_next = Level.second
+    @require_point = @level_next.required_gold - @level_current.required_gold
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/games/result', to: 'games#result'
   get '/games', to: 'games#index'
   get '/trial', to: 'trials#index'
+  get '/trial/result', to: 'trials#result'
 
   namespace :admin do 
     resources :monsters
